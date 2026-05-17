71bf1eeb205b598c88acd987165886a90825e776
Tisha Dey <tishadey2025@outlook.com>
2026-05-17 21:29:18 +0600

    :tada: fixing cors issue

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors(
+  ({
+    origin: ["http://localhost:3000","https://hope-fund-theta.vercel.app"],
+    credentials: true,
+  })
+));
 app.use(express.json());
 app.use(cookieParser());
 
