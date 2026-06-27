083152320579e601502133dc2e4a466b2e0b633b
Tisha Dey <tishadey2025@outlook.com>
2026-06-27 16:38:40 +0600

    cloudinary file upload feature added

--- index.js
+++ index.js
@@ -3,19 +3,35 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+const fs = require("fs-extra");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors(
-  ({
-    origin: ["http://localhost:3000","https://hope-fund-theta.vercel.app"],
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://hope-fund-theta.vercel.app"],
     credentials: true,
-  })
-));
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
+
+// cloudinary configuration
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+//multer configuration
+const upload = multer({
+  dest: "uploads/",
+});
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d2ts7wd.mongodb.net/?appName=Cluster0`;
@@ -30,21 +46,23 @@ const client = new MongoClient(uri, {
 });
 
 //verify token
-function verifyToken(req,res,next){
-const token = req.cookies?.token;
-// console.log(token);
-if(!token){
-  return res.status(401).send({error: true, message: "unauthorized access"});
-}
-jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-  if (err) {
-    return res.status(403).send({error: true, message: "forbidden access"});
+function verifyToken(req, res, next) {
+  const token = req.cookies?.token;
+  // console.log(token);
+  if (!token) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
   }
-  req.decoded = decoded;
-  next();
-});
-
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ error: true, message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
 }
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -74,11 +92,31 @@ async function run() {
     });
 
     // create a add campaign
-    app.post("/add-campaign", async (req, res) => {
-      const donation = req.body;
-      const result = await campaignCollection.insertOne(donation);
-      res.send(result);
-    });
+    app.post(
+      "/add-campaign",
+      upload.single("campaign_photo"),
+      async (req, res) => {
+        try {
+          const campaign_photo = await cloudinary.uploader.upload(
+            req.file.path,
+            {
+              folder: "donation-campaign-platform",
+            },
+          );
+          await fs.remove(req.file.path);
+          req.body.campaign_photo = campaign_photo.secure_url;
+          req.body.organizer = JSON.parse(req.body.organizer);
+          const donation = req.body;
+          const result = await campaignCollection.insertOne(donation);
+          res.send(result);
+        } catch (error) {
+          console.error("Error occurred while adding campaign:", error);
+          res
+            .status(500)
+            .send({ error: true, message: "Internal server error" });
+        }
+      },
+    );
 
     //get all campaigns
     app.get("/campaigns", async (req, res) => {
@@ -89,7 +127,7 @@ async function run() {
     //get single campaign
     app.get("/campaign/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
+      // console.log(id);
       const query = { _id: new ObjectId(id) };
       // console.log(query);
       const result = await campaignCollection.findOne(query);
@@ -99,7 +137,7 @@ async function run() {
     //delete campaign
     app.delete("/campaign/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
+      // console.log(id);
       const query = { _id: new ObjectId(id) };
       const result = await campaignCollection.deleteOne(query);
       res.send(result);
